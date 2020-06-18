49e58931751f6dbd93e56e34db1ad0825509aece
Aditya Malviya <malviya.et@gmail.com>
2020-06-18 23:28:13 +0530

    Update Demo1Application.java

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -13,7 +13,7 @@ public class Demo1Application {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Greetings from Nuclear Geeks Dev";
+		return "Greetings from Nuclear Geeks QA";
 	}
 
 
